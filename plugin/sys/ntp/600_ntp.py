b4d1a147889fd5215ed1e00ff6fad390466b6c2c
sht401 <35591214+sht401@users.noreply.github.com>
2018-06-12 14:49:15 +0800

    Update 600_ntp.py

--- plugin/sys/ntp/600_ntp.py
+++ plugin/sys/ntp/600_ntp.py
@@ -1,21 +1,8 @@
-#!-*- coding:utf8 -*-
 
-import requests
 import time
 import json
 
-ts = int(time.time())
-payload = [
-    {
-        "endpoint": "siplug",
-        "metric": "plugtest",
-        "timestamp": ts,
-        "step": 60,
-        "value": 0.123,
-        "counterType": "GAUGE",
-        "tags": "idc=lg,loc=beijing",
-    },
-]
 
-r = requests.post("http://192.168.115.49:1988/v1/push", data=json.dumps(payload))
-print r.text
+output = [{"endpoint": "monitor-test-centos", "tags": "", "timestamp": int(time.time()), "metric": "agent.cpu", "value": 1.8, "counterType": "GAUGE", "step": 60}]
+
+print  json.dumps(output)
