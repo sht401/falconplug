5740036033107dcd957984e2dea075bd5ea8a92c
sht401 <35591214+sht401@users.noreply.github.com>
2018-06-12 13:56:10 +0800

    sss
    sss

---
+++ plugin/sitest/60_sss.py
@@ -0,0 +1,20 @@
+#!-*- coding:utf8 -*-
+
+import requests
+import time
+import json
+
+ts = int(time.time())
+payload = [
+    {
+        "endpoint": "siplug",
+        "metric": "plugtest",
+        "timestamp": ts,
+        "step": 60,
+        "value": 3,
+        "counterType": "GAUGE",
+        "tags": "idc=lg,loc=beijing",
+    },
+]
+
+r = requests.post("http://192.168.115.49:1988/v1/push", data=json.dumps(payload))
