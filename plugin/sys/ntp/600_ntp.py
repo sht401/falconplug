8722b708cec6f66a9e20810c3ad5d1ba0d8dedb2
sht401 <35591214+sht401@users.noreply.github.com>
2018-06-12 14:43:13 +0800

    edit

--- plugin/sys/ntp/600_ntp.py
+++ plugin/sys/ntp/600_ntp.py
@@ -1,1 +1,21 @@
-[{"endpoint": "host01", "tags": "", "timestamp": 1431349763, "metric": "sys.ntp.offset", "value": 0.73699999999999999, "counterType": "GAUGE", "step": 600}]
+#!-*- coding:utf8 -*-
+
+import requests
+import time
+import json
+
+ts = int(time.time())
+payload = [
+    {
+        "endpoint": "siplug",
+        "metric": "plugtest",
+        "timestamp": ts,
+        "step": 60,
+        "value": 0.123,
+        "counterType": "GAUGE",
+        "tags": "idc=lg,loc=beijing",
+    },
+]
+
+r = requests.post("http://192.168.115.49:1988/v1/push", data=json.dumps(payload))
+print r.text
